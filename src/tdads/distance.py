3450a16c568b10f19197aaacd77901dc47cc0b4f
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 <shaelebrown@gmail.com>
2024-06-26 15:37:34 -0400

    Wrote FIM distance, all tests pass

--- src/tdads/distance.py
+++ src/tdads/distance.py
@@ -1,10 +1,19 @@
 
 # functions to calculate distances between diagrams
 from tdads.diagram_utils import *
+from scipy.optimize import linear_sum_assignment
+from scipy.spatial.distance import cdist
+from numpy import concatenate, array, logical_or, exp, sqrt, arccos
+from math import pi
 
 # add in extra parameter for n_cores in distance constructor
 class distance:
-    def __init__(self, metric='W',p=2, sigma=None):
+    def __init__(self, dim = 0,metric='W',p=2, sigma=None, n_cores=1):
+        if not isinstance(dim,type(2)):
+            raise Exception('dim must be an integer.')
+        if dim < 0:
+            raise Exception('dim must be non-negative.')
+        self.dim = dim
         if not isinstance(metric,type('')):
             raise Exception('The distance metric must be a string.')
         if metric != 'W' and metric != 'FIM':
@@ -24,6 +33,12 @@ class distance:
                 raise Exception('For the Fisher information metric sigma must be positive.')
             self.sigma = sigma
             self.p = None
+        if not isinstance(n_cores,type(2)):
+            raise Exception('n_cores must be an integer.')
+        if n_cores < 0:
+            raise Exception('n_cores must be non-negative.')
+        # then check if n_cores is more than the number of available cores
+        self.n_cores = n_cores
     def __str__(self):
         if self.sigma != None:
             M = 'Fisher information metric with parameter sigma = ' + str(self.sigma) + '.'
@@ -35,6 +50,80 @@ class distance:
         return M
     def compute(self,D1,D2):
         '''Compute a single distance value.'''
-        D1 = preprocess_diagram(D1)
-        D2 = preprocess_diagram(D2)
-        return 1
+        # preprocess diagrams
+        D1 = preprocess_diagram(D1, ret=True)
+        D2 = preprocess_diagram(D2, ret=True)
+        # subset diagrams to correct dimensions
+        D1_sub = D1[self.dim]
+        D2_sub = D2[self.dim]
+        # avoid infinity's in diagrams
+        if float('inf') in D1_sub[:,1] or float('inf') in D2_sub[:,1]:
+            raise Exception('Infinity value found in a diagram - infinities should be removed prior to distance calculations.')
+        # remove diagonal points from both diagrams
+        D1_sub = D1_sub[(D1_sub[:,0] < D1_sub[:,1]),:]
+        D2_sub = D2_sub[(D2_sub[:,0] < D2_sub[:,1]),:]
+        # check for empty diagrams, return distance 0
+        if D1_sub.shape[0] == 0 and D2_sub.shape[0] == 0:
+            return 0
+        # compute diagonal projections and concatenate to opposite diagram
+        if D1_sub.shape[0] > 0:
+            diag1 = concatenate([0.5*array([x[0]+x[1], x[0]+x[1]]).reshape((1,2)) for x in D1_sub])
+            n_diag1 = diag1.shape[0]
+        else:
+            diag1 = empty((0,2))
+            n_diag1 = 0
+        if D2_sub.shape[0] > 0:
+            diag2 = concatenate([0.5*array([x[0]+x[1], x[0]+x[1]]).reshape((1,2)) for x in D2_sub])
+            n_diag2 = diag2.shape[0]
+        else:
+            diag2 = empty((0,2))
+            n_diag2 = 0
+        D2_sub = concatenate([D2_sub, diag1])
+        D1_sub = concatenate([D1_sub, diag2])
+        # compute distance
+        if self.metric == 'W':
+            # wasserstein/bottleneck distance
+            # calculate max (chebyshev) distance between each pair of rows in D1_sub and D2_sub
+            dist_mat = cdist(D1_sub, D2_sub, metric='chebyshev')
+            # set distances between points and their projections to 0
+            v1 = D1_sub.shape[0] - n_diag2
+            v2 = D2_sub.shape[0] - n_diag1
+            if v1 < D1_sub.shape[0] and v2 < D2_sub.shape[0]:
+                dist_mat[range(D1_sub.shape[0] - n_diag2, D1_sub.shape[0]),range(D2_sub.shape[0] - n_diag1, D2_sub.shape[0])] = 0
+            # if wasserstein distance then exponentiate
+            if self.p < float('inf'):
+                dist_mat = dist_mat**self.p
+            # solve linear sum assignment problem
+            row_ind, col_ind = linear_sum_assignment(cost_matrix = dist_mat, maximize = False)
+            # subset to remove matches between projection points
+            good_inds = logical_or(row_ind < D1_sub.shape[0] - n_diag2, col_ind < D2_sub.shape[0] - n_diag1)
+            row_ind = row_ind[good_inds]
+            col_ind = col_ind[good_inds]
+            # return distance value
+            if self.p < float('inf'):
+                return dist_mat[row_ind, col_ind].sum()**(1/self.p)
+            else:
+                return dist_mat[row_ind, col_ind].max()
+        else:
+            # Fisher information metric distance
+            theta = concatenate([D1_sub, D2_sub])
+            # exact calculation for now...
+            rho1 = [exp(-1*cdist(x.reshape((1,2)), D1_sub, metric='euclidean')**2/(2*self.sigma**2)).sum()/(sqrt(2*pi)*self.sigma) for x in theta]
+            rho2 = [exp(-1*cdist(x.reshape((1,2)), D2_sub, metric='euclidean')**2/(2*self.sigma**2)).sum()/(sqrt(2*pi)*self.sigma) for x in theta]
+            # check for same vectors
+            if rho1 == rho2:
+                return 0
+            # normalize
+            rho1 = rho1/sum(rho1)
+            rho2 = rho2/sum(rho2)
+            # dot product
+            norm = sum([sqrt(x)*sqrt(y) for x,y in zip(rho1, rho2)])
+            # check bounds and return arc cos
+            if norm > 1:
+                norm = 1
+            if norm < -1:
+                norm = -1
+            return arccos(norm)
+
+        
+        
--- tests/test_distance.py
+++ tests/test_distance.py
@@ -6,6 +6,7 @@ from numpy import array, inf, equal
 from numpy.random import random
 from ripser import ripser
 import gudhi
+from math import sqrt
 
 def test_distance_class():
     '''Test class constructor and other basic methods.'''
@@ -21,16 +22,69 @@ def test_distance_class():
         dist = distance(metric = 'FIM',sigma='2')
     with pytest.raises(Exception, match = 'For the Fisher information metric sigma must be positive.'):
         dist = distance(metric = 'FIM',sigma=-0.5)
+    with pytest.raises(Exception, match = 'dim'):
+        dist = distance(dim='1')
+    with pytest.raises(Exception, match = 'dim'):
+        dist = distance(dim=1.1)
+    with pytest.raises(Exception, match = 'dim'):
+        dist = distance(dim=-1)
+    with pytest.raises(Exception, match = 'n_cores'):
+        dist = distance(n_cores='1')
+    with pytest.raises(Exception, match = 'n_cores'):
+        dist = distance(n_cores=1.1)
+    with pytest.raises(Exception, match = 'n_cores'):
+        dist = distance(n_cores=-1)
     dist = distance()
     assert dist.__str__() == '2-wasserstein distance.'
     dist = distance(metric = 'FIM', sigma = 2)
     assert dist.__str__() == 'Fisher information metric with parameter sigma = 2.'
     dist = distance(p = float('inf'))
     assert dist.__str__() == 'Bottleneck distance.'
-    rips = gudhi.RipsComplex(points=[[0, 0], [1, 0], [0, 1], [1, 1]], max_edge_length=42)
-    simplex_tree = rips.create_simplex_tree(max_dimension=1)
-    diag1 = simplex_tree.persistence(homology_coeff_field=2, min_persistence=0)
-    diag2 = [array([[0,1],[1,2]]),array([[1,2],[3,4]])]
-    assert distance().compute(diag1,diag2) == 1 # for now
+    diag = [array([[0,1],[1,2]]),array([[1,2],[3,4]])]
     with pytest.raises(Exception, match = 'Diagrams must be computed from either the ripser, gph, flagser, gudhi or cechmat libraries.'):
-        dist.compute(diag1, 1)
+        dist.compute(diag, 1)
+    
+def test_distance_calculations():
+    '''For wasserstein, bottleneck, FIM and parallelized distance mats'''
+    dist_w2 = distance()
+    dist_w3 = distance(p = 3)
+    dist_b = distance(p = float('inf'))
+    dist_FIM1 = distance(metric = 'FIM', sigma = 1)
+    D1 = [array([2,3]).reshape(1,2)]
+    D2 = [array([2,3.1,5,6]).reshape(2,2)]
+    assert dist_w2.compute(D1,D2) == sqrt(0.1**2 + 0.5**2)
+    assert dist_w3.compute(D1,D2) == (0.1**3 + 0.5**3)**(1/3)
+    assert dist_b.compute(D1,D2) == 0.5
+    assert dist_w2.compute(D2,D1) == sqrt(0.1**2 + 0.5**2)
+    assert dist_w3.compute(D2,D1) == (0.1**3 + 0.5**3)**(1/3)
+    assert dist_b.compute(D2,D1) == 0.5
+    assert dist_w2.compute(D1,D1) == 0
+    assert dist_w3.compute(D1,D1) == 0
+    assert dist_b.compute(D1,D1) == 0
+    assert dist_w2.compute(D2,D2) == 0
+    assert dist_w3.compute(D2,D2) == 0
+    assert dist_b.compute(D2,D2) == 0
+    D2 = [empty((0,2)),array([2,3.1,5,6]).reshape(2,2)]
+    assert dist_w2.compute(D1,D2) == 0.5
+    assert dist_w3.compute(D1,D2) == 0.5
+    assert dist_b.compute(D1,D2) == 0.5
+    assert dist_w2.compute(D2,D1) == 0.5
+    assert dist_w3.compute(D2,D1) == 0.5
+    assert dist_b.compute(D2,D1) == 0.5
+    D2 = [array([2,3.1,5,6]).reshape(2,2)]
+    assert dist_FIM1.compute(D1,D2) == pytest.approx(dist_FIM1.compute(D2,D1))
+    assert dist_FIM1.compute(D1,D1) == 0
+    assert dist_FIM1.compute(D2,D2) == 0
+    D1 = [array([2,3]).reshape((1,2))]
+    D2 = [array([[2,3.3],[0,0.5]])]
+    D3 = [array([0,0.5]).reshape((1,2))]
+    assert dist_w2.compute(D1,D2) == pytest.approx(sqrt(0.1525))
+    assert dist_b.compute(D1,D2) == pytest.approx(0.3)
+    assert dist_w2.compute(D3,D1) == pytest.approx(sqrt(0.3125))
+    assert dist_b.compute(D3,D1) == pytest.approx(0.5)
+    assert dist_w2.compute(D2,D3) == pytest.approx(0.65)
+    assert dist_b.compute(D2,D3) == pytest.approx(0.65)
+    assert dist_FIM1.compute(D1,D2) == pytest.approx(0.02354624, abs=1e-4)
+    assert dist_FIM1.compute(D1,D3) == pytest.approx(0.08821907, abs=1e-4)
+    assert dist_FIM1.compute(D3,D2) == pytest.approx(0.08741134, abs=1e-4)
+
