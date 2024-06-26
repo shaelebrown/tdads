9db131a90ef2c338a78814566ba62517070721d6
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 <shaelebrown@gmail.com>
2024-06-25 20:45:32 -0400

    Removed unhelpful comment

--- src/tdads/__init__.py
+++ src/tdads/__init__.py
@@ -2,7 +2,7 @@
 from importlib.metadata import version
 __version__ = version("tdads")
 
-
+# only include public functions here
 from .diagram_utils import check_diagram, preprocess_diagram
 
 __all__ = [
